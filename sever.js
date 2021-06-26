61def7d3ec55255fd09c2b4f8ddd40dbc88f4685
fabricio3g <fabricio.3g@gmail.com>
2021-06-26 11:20:58 -0300

    first commit

---
+++ sever.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const multer = require('multer')
+
+
+
+const app = express()
+
+app(cors())
+
+
+app.get('/', (req, res)=>{
+    res.sendFile('index.html')
+})
+
+app.post('/api/fileanalysis', multer.single('upfile'), (req, res)=>{
+    
+    console.log(req.file)
+
+    res.json({})
+})
+
+app.listen(3000, (req, res) =>{
+    console.log('Server running')
+})
